9271d868aca9ca8f7aea331a10304e4fc1ffe0be
faker2048 <nspyia2002@gmail.com>
2023-05-07 23:16:58 +0800

    use loguru

--- wstp/main.py
+++ wstp/main.py
@@ -1,22 +1,15 @@
-# main.py
 import sys
 import time
 import keyboard
+import os
+from loguru import logger
 from speech_recorder import SpeechRecorder
+from speech2text import speech2text
 
 
 def print_file_size(file_path: str):
-    import os
-
     file_size = os.path.getsize(file_path)
-    print(f"file_size: {file_size / 1024 / 1024:.2f} MB")
-
-
-def speech2text(audio_file: str) -> str:
-    # 实现speech2text的具体逻辑
-    # 返回 audio_file 的时长和文件大小
-    print(f"audio_file: {audio_file}")
-    pass
+    logger.info("File size: {:.2f} MB".format(file_size / 1024 / 1024))
 
 
 def main():
@@ -24,22 +17,21 @@ def main():
 
     while True:
         if keyboard.is_pressed("i"):
-            print("开始录音...")
+            logger.info("Recording started...")
             recorder.start_recording()
             while keyboard.is_pressed("i"):
                 recorder.record()
                 time.sleep(0.01)
-            print("停止录音...")
+            logger.info("Recording stopped...")
             audio_file = recorder.stop_recording()
             text = recorder.process_audio(audio_file)
-            print(f"转换结果：{text}")
+            logger.info("Conversion result: {}".format(text))
 
         if keyboard.is_pressed("q"):
-            print("退出程序...")
+            logger.info("Exiting program...")
             sys.exit(0)
-
-        time.sleep(0.1)
 
 
 if __name__ == "__main__":
+    logger.info("Starting program...")
     main()
---
+++ wstp/speech2text.py
@@ -0,0 +1,6 @@
+from loguru import logger
+
+
+def speech2text(audio_file: str) -> str:
+    logger.info("Converting speech to text...")
+    # Add your code for converting speech to text here
