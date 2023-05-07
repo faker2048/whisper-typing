a50268406d87418d5d0022b378285d35b5f7bd78
faker2048 <nspyia2002@gmail.com>
2023-05-08 00:12:04 +0800

    Add parse_args for main

--- wstp/main.py
+++ wstp/main.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import keyboard
+import argparse
 from loguru import logger
 from speech_recorder import SpeechRecorder
 
@@ -17,8 +18,6 @@ def handle_record(keyboard_key: str, recorder: SpeechRecorder):
     recorder.stop_recording()
     audio_file = recorder.save_audio("target/audio.wav")
     logger.info("Audio file saved: {}".format(audio_file))
-    text = recorder.process_audio(audio_file)
-    logger.info("Conversion result: {}".format(text))
 
 
 def handle_exit():
@@ -26,13 +25,40 @@ def handle_exit():
     sys.exit(0)
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Speech Recorder")
+    parser.add_argument(
+        "-r", "--rate", type=int, default=16000, help="Sampling rate (default: 16000)"
+    )
+    parser.add_argument(
+        "-b",
+        "--buffer-size",
+        type=int,
+        default=1024,
+        help="Buffer size (default: 1024)",
+    )
+    parser.add_argument(
+        "-k",
+        "--keyboard-key",
+        type=str,
+        default="i",
+        help="Keyboard key to start/stop recording (default: 'i')",
+    )
+    return parser.parse_args()
+
+
 def main():
-    recorder = SpeechRecorder()
+    args = parse_args()
+
+    recorder = SpeechRecorder(
+        rate=args.rate,
+        buffer_size=args.buffer_size,
+    )
 
     while True:
-        keyboard.wait("i")
-        if keyboard.is_pressed("i"):
-            handle_record("i", recorder)
+        keyboard.wait(args.keyboard_key)
+        if keyboard.is_pressed(args.keyboard_key):
+            handle_record(args.keyboard_key, recorder)
         if keyboard.is_pressed("q"):
             handle_exit()
         time.sleep(0.1)
